e92a1b3b39350d49b052aceddf664fad1503066a
Tamilarasan S <tarasan833@gmail.com>
2022-05-12 18:32:48 +0530

    verifycard component added

--- src/app/payment/payment-routing.module.ts
+++ src/app/payment/payment-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CardpaymentComponent } from './cardpayment/cardpayment.component';
+import { VerifycardComponent } from './verifycard/verifycard.component';
 
 const routes: Routes = [
-  {
-    path:'cardpayment',component:CardpaymentComponent
-  }
+  { path:'cardpayment',component:CardpaymentComponent},
+  {path:'verifycard',component:VerifycardComponent,}
 ];
 
 @NgModule({
