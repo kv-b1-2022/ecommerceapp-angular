cdda8b3fe325784134a7235474ed8a5acac7b5dc
Arun S <101079723+arun-gits@users.noreply.github.com>
2022-05-16 11:44:13 +0530

    added authentication for header and sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ecommerceapp-ui';
+
+  getUser(){
+    let user = localStorage.getItem('LOGGED_IN_USER');
+    if(user != null){
+      return JSON.parse(user);
+    }
+    return null;
+  }
+  getStyle(){
+    let clazz = '';
+    let role = this.getUser()?.role;
+    if(role=='admin'){
+      clazz = 'sidebar-open';
+    }
+    return clazz;
+  }
 }
