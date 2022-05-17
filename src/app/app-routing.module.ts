37e6fdd63535bdad38e3dd2776bcbe2ada386d4c
kishore <you@example.comgit>
2022-05-17 16:09:52 +0530

    added transaction module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DisplaybookComponent } from './books/displaybook/displaybook.component';
 import { HomeComponent } from './home/home.component';
+import { TransactiondetailsComponent } from './transaction/transactiondetails/transactiondetails.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'transaction', component: TransactiondetailsComponent},
 
   
   
