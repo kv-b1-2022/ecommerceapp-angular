570c4ded45d6df109550c6d03c540243663ddd87
Monisha Govindaraju <rg.monisha105@gmail.com>
2022-05-17 12:43:11 +0530

    donation app angular

--- src/app/charity/donate/donate.component.ts
+++ src/app/charity/donate/donate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-donate',
@@ -8,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonateComponent implements OnInit {
 
-  constructor() { }
+  donationamount!: string;
+  donationcategories!: string;
+  
+  constructor(private http:HttpClient) {}
+   
 
   ngOnInit(): void {
-  }
 
+  }
+  donate() {
+     const userObj={
+      
+      "donationamount":this.donationamount,
+      "donationcategories":this.donationcategories,
+
+     };
+     console.log(userObj);
+     const url="http://localhost:9000/donate/save";
+     this.http.post(url,userObj).subscribe((res)=>{
+       console.log(res);
+       alert("You have donated successfully");
+     },(err)=>{
+       console.log(err);
+       alert("Please donate again");
+     })
+  }
 }
