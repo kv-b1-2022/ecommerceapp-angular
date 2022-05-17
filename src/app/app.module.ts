47ad4eb616f2124d5cd392c0922f9158d07118c6
Murugan2207 <vickyvenkat2207@gmail.com>
2022-05-17 14:48:21 +0530

    added movie features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -78,7 +78,6 @@ import { ElectronicsModule } from './Electronics/electronics.module';
     EmployeesModule,
     OrderModule,
     PaymentModule,
-    NgChartsModule,
     CoursesModule,
     MembershipModule,
     PaymentModule,
