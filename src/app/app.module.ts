72391a68bddb344d18219a60117921f91c117c8d
perianangit <periananpdkt@gmail.com>
2022-05-17 12:47:05 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { MoviesModule } from './movies/movies.module';
 import { MedicalModule } from './medical/medical.module';
 
 import { FoodsModule } from './foods/foods.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { GroceryModule } from './grocery/grocery.module';
@@ -26,11 +25,7 @@ import { OrderModule } from './order/order.module';
 
 import { CoursesModule } from './courses/courses.module';
 import { EmployeesModule } from './employees/employees.module';
-<<<<<<< HEAD
-import { MembershipModule } from './membership/membership.module';
-=======
 import { MembershipModule } from './membership/membership.module'; 
->>>>>>> fb5a1a72ec77e53889f626f02aa0e019abd49877
 import { StockModule } from './stock/stock.module';
 import { PayrollModule } from './payroll/payroll.module';
 
@@ -45,14 +40,11 @@ import { AdminSidebarComponent } from './admin-sidebar/admin-sidebar.component';
 import { AuthModule } from './auth/auth.module';
 
 import { BooksModule } from './books/books.module';
-<<<<<<< HEAD
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-=======
 import { EmployeeProfilepageComponent } from './employee-profilepage/employee-profilepage.component';
 import { ElectronicsModule } from './Electronics/electronics.module';
 
 
->>>>>>> fb5a1a72ec77e53889f626f02aa0e019abd49877
 
 
 @NgModule({
